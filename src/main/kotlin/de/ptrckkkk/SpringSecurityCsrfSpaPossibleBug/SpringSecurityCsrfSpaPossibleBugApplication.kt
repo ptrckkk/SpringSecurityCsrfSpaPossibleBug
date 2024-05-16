90677cb8466334750eb8dadec0daf2fb5f91e2da
patrick <patrick-mell@web.de>
2024-05-16 19:45:26 +0200

    Add project as created by the Spring Initializr

---
+++ src/main/kotlin/de/ptrckkkk/SpringSecurityCsrfSpaPossibleBug/SpringSecurityCsrfSpaPossibleBugApplication.kt
@@ -0,0 +1,11 @@
+package de.ptrckkkk.SpringSecurityCsrfSpaPossibleBug
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringSecurityCsrfSpaPossibleBugApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringSecurityCsrfSpaPossibleBugApplication>(*args)
+}
---
+++ src/test/kotlin/de/ptrckkkk/SpringSecurityCsrfSpaPossibleBug/SpringSecurityCsrfSpaPossibleBugApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.ptrckkkk.SpringSecurityCsrfSpaPossibleBug
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringSecurityCsrfSpaPossibleBugApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
